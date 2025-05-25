b7a9e0f9927b4f77a1f403938ce2eb6781598457
Natasha <allthingssoftwarewithmadhlomo@gmail.com>
2025-05-25 22:53:00 +0200

    Fix: Add multiplication operation to basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -3,9 +3,9 @@ number2 = 5
 
 addition = number1 + number2
 subtraction = number1 - number2
-multiplication = number1 + number2
+multiplication = number1 * number2
 
-print(f"Addition f {number1} and {number2} is {addition}")
+print(f"Addition of {number1} and {number2} is {addition}")
 print(f"Subtraction of {number1} and {number2} is {subtraction}")
 print(f"Multiplication of {number1} and {number2} is {multiplication}")
 
